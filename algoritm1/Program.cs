036a8c2b23532c752cca1d3fe45a0479be1ff31f
jalya07 <jalavh@code.edu.az>
2025-10-04 17:50:21 +0400

    add files

---
+++ algoritm1/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace algoritm1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        //task1
+        //int a = 4;
+        //int b = 3;
+        //int c = 5;
+        //if (((a * a) + (b * b)) == (c*c))
+
+        //    Console.WriteLine("Pryamouqolniy treuqolnik");
+
+        //else
+        //    Console.WriteLine("ne pryamouqolniy treuqolnik");
+
+
+        //task2
+        //string s = "Salam";
+        //int m = s.Length;
+        //if (m % 2 == 1)
+        //    Console.WriteLine(s[m / 2]);
+        //else
+        //    Console.WriteLine(s[m / 2 - 1].ToString() + s[m / 2].ToString());
+
+
+        //task3
+        //int n = 378;
+        //int sum = 0;
+        //while (n>0)
+        //{
+        //    int m = n % 10;
+        //    if (m % 2 == 1)
+        //    {
+        //        sum += m;
+        //    }
+        //    n = n / 10;
+        //}
+        //Console.WriteLine(sum);
+
+        //task4
+        int x = 100;
+        int y = 30;
+        int max = -1;
+        for (int i = 0; i <x; i++)
+        {
+            if (i % y == 0)
+            {
+                max = i;
+            }
+        }
+        Console.WriteLine(max);
+
+
+
+    }
+}
+
