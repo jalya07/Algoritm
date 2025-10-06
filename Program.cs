e55cfeca0dd8035442ebb5b2f3b0011a6ecfa946
jalya07 <jalavh@code.edu.az>
2025-10-06 11:23:26 +0400

    add files

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿namespace algoritm2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //task1
+        //int[] n = { 50, 40, 30, 20, 10 };
+        //int a = 50;
+        //int index = -1;
+        //for (int i = 0; i < n.Length; i++)
+        //{
+        //    if (n[i] == a)
+        //    {
+        //        index = i;
+        //        break;
+        //    }
+        //}
+        //Console.WriteLine(index);
+
+        //task2
+        //int[] n = { 1, 2, 3, 2, 4, 2 };
+        //int count = 0;
+        //for (int i = 0; i < n.Length; i++)
+        //{
+        //    for (int j = i + 1; j < n.Length; j++)
+        //    {
+        //        if (n[i] == n[j])
+        //        {
+        //            count++;
+        //        }
+        //    }
+        //}
+        //Console.WriteLine(count);
+
+        //task3
+        //string s = "vsem salam";
+        //int max = 0;
+        //char simvol = ' ';
+        //for(int i = 0; i < s.Length; i++)
+        //{
+        //    int count = 0;
+        //    for(int j = 0; j < s.Length; j++)
+        //    {
+        //        if (s[i] == s[j])
+        //            count++;
+        //    }
+        //    if (count > max)
+        //    {
+        //        max = count;
+        //        simvol = s[i];
+        //    }
+        //}
+        //Console.WriteLine(simvol);
+        //Console.WriteLine(max);
+
+
+
+        //task4
+        //int n = 100;
+        //for(int i = 0; i < n; i++)
+        //{
+        //    int number = i;
+        //    int temp = number;
+        //    int r = 0;
+        //    while (temp > 0)
+        //    {
+        //        int m = temp % 10;
+        //        r = (r * 10) + m;
+        //        temp = temp / 10;
+        //    }
+        //    if (number  == r)
+        //    {
+        //        Console.WriteLine(r);
+        //    }
+        //}
+
+    }
+}
+
