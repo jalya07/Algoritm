52592ce3d77e40b28892a6db44b8fe4fc9479cc4
jalya07 <jalavh@code.edu.az>
2025-10-06 12:12:30 +0400

    add files

---
+++ lab1/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+namespace ConditionExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task1
+            //int n = 5;
+            //if (n > 0)
+            //{
+            //    Console.WriteLine("positive");
+            //}
+
+            //else if (n < 0)
+            //{
+            //    Console.WriteLine("neqative");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("zero");
+            //}
+
+
+
+            //task2
+            //int n = 8;
+            //if (n % 2 == 0)
+            //{
+            //    Console.WriteLine("cetnoe");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("necetnoe");
+            //}
+
+
+
+            //task3
+            //int n = 5;
+            //int m = 7;
+            //if (n > m)
+            //    Console.WriteLine("n max");
+            //else
+            //    Console.WriteLine("m max");
+
+
+
+
+            //task4
+            //int n = 8;
+            //int m = 7;
+            //int a = 9;
+            //if (n > m)
+            //{
+            //    if (n > a)
+            //    {
+            //        Console.WriteLine("n max");
+
+            //    }
+            //    else
+            //        Console.WriteLine("a max");
+            //}
+            //else
+            //{
+            //    if (m > a)
+            //    {
+            //        Console.WriteLine("m max");
+
+            //    }
+            //    else
+            //        Console.WriteLine("a max");
+            //}
+
+
+            //task5
+            //int n = 65;
+            //switch (n)
+            //{
+            //    case n>90:
+            //        Console.WriteLine("a");
+            //    case n > 80:
+            //        Console.WriteLine("b");
+            //    case n > 70:
+            //        Console.WriteLine("c");
+            //    case n > 60:
+            //        Console.WriteLine("d");
+
+
+            //task6
+
+            //int a = 9;
+            //int b = 8;
+            //int c = 18;
+            //if (a + b > c && a + c > b && c + b > a)
+            //{
+            //    Console.WriteLine("triangle");
+            //}
+            //else
+            //    Console.WriteLine("no triangle");
+
+
+
+            //task7
+            //int n = 798;
+            //int a = n / 10;
+            //int d = a % 10;
+            //int b = a / 10;
+            //int c = n % 10;
+            //Console.WriteLine(d+b+c);
+
+
+
+
+
+
+
+            //var n = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
